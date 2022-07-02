0ea35f0f45fe7e8cf6c675002f66a6548c047b67
steve-d-code <clndre33@gmail.com>
2022-07-02 18:05:00 -0400

    updated port

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ app.use(express.static(__dirname + "/public"));
 
 
 app.get('/', (req, res) => {
-    res.send("UPDATE 070222 5:47: <a href='public/index.html'>index</a>");
+    res.send("UPDATE 070222 6:03: <a href='public/index.html'>index</a>");
 });
 
 
-const port = process.env.port || 3000;
+const port = process.env.port || 8080;
 app.listen(port, () => {
     console.log("Stan the solidity");
 });
